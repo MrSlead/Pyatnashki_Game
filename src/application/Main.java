d53d6beb33c7530e05e5eb0a4360894388874a63
Alexo <mordyasov.60@mail.ru>
2020-06-13 00:06:12 +0700

    add comments in 2 classes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,18 @@
 package application;
-	
+
+/**
+ * Декстопное приложение "Пятнашки" написано с использованием JavaFX + Scene Builder.
+ * Смысл игры заключается в том, чтобы разместить числа по порядку слева направо.
+ * 
+ * Состоит из 1 fxml файла, содержащего 3 pane, одно из которых является родительским
+ * для всех компонентов, другое для таймера, последнее для шагов.
+ * Кнопка начала игры - startButton, Текст - "Шаг", MenuButton - выбор размерности игры,
+ * Текст - время, пройденное после начала игры.
+ * 
+ * @author Alex
+ * @version 1.0
+ */
+
 import java.util.Map;
 import event.TimerEvent;
 import javafx.application.Application;
@@ -10,6 +23,7 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	/** Мап, хранящий все fxml элементы */
 	private static Map<String, Object> fxmlNamespace;
 	
 	@Override
@@ -17,13 +31,16 @@ public class Main extends Application {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/resource/main.fxml"));
 			fxmlNamespace = loader.getNamespace();
+			
 			Pane root = (Pane) loader.load();
 			Scene scene = new Scene(root, 700, 600);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Пятнашки");
 			primaryStage.setResizable(false);
 			
+			/** Остановить таймер времени после закрытия приложения */
 			primaryStage.setOnCloseRequest(e -> TimerEvent.stopTime());
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -5,29 +5,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import application.Main;
-import event.ButtonEvent;
-import event.GridItemEvent;
 import event.MenuButtonEvent;
 import event.TimerEvent;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SplitMenuButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.control.MenuButton;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
+/**
+ * Класс MainController является контроллером графического api, содержащий все элементы fxml.
+ * @author Alex
+ * @version 1.0
+ */
 public class MainController implements Initializable  {
 
 	@FXML
@@ -60,6 +55,11 @@ public class MainController implements Initializable  {
 	private List<MenuItem> items;
 	
     
+	/** 
+	 * При нажатии на кнопку НАЧАЛО скрывается эта кнопка и меню с выбором размерности игры.
+	 * Появляется pane с текстом ШАГ и pane с текстом времени.
+	 * @param event - сихронизация метода с нажатием на кнопку установлена в main.fxml "onAction="#clickStartButton""
+	 */
 	@FXML
 	private void clickStartButton(ActionEvent event) {
 		if(!gridPane.getChildren().isEmpty()) {
@@ -69,6 +69,7 @@ public class MainController implements Initializable  {
 			timePane.setVisible(true);
 			TimerEvent.startTime();
 			
+			/** Снимается блокировка с кнопок, поэтому на них можно будет нажимать. */
 			gridPane.getChildren()
 			.stream()
 			.forEach(item ->
@@ -76,19 +77,24 @@ public class MainController implements Initializable  {
 		}
 	}
 
+	/** При выборе режима 3x3 или другого фомируются на окне выбранная размерность кнопок в случайном порядке. */
 	@FXML
 	private void clickMenuItem() {
 		MenuButtonEvent.setTextAndGenerateButton();
 	}
 	
+	
+	/** Инициализируется во время открытия данного окна, так как это окно открывается при запуске программы, то инициализируется  
+	 * во время запуска программы. */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		/** Режимы для игры, размерность */
 		items = new ArrayList<>(
 				Arrays.asList(
 						new MenuItem("3x3"),
 						new MenuItem("4x4"),
 						new MenuItem("5x5")
-		// new MenuItem("6x6")
+						// new MenuItem("6x6")
 		));
 
 		menuButton.getItems().addAll(items);
